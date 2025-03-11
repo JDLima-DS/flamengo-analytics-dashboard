a78086e3302c5d3323cea97614b749803112f665
JDLima-DS <j.davi4242@gmail.com>
2025-03-10 23:30:37 -0300

    add flamengo databases from @williamjouse and load data

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import streamlit as st
+import numpy as np
+
+df_goals = pd.read_csv("databases/goal_scorers_2024_processed.csv")
+print(df_goals.head())
+print("===================================")
+df_stats = pd.read_csv("databases/stats_FLA2024_processed.csv")
+print(df_stats.head())
+print("===================================")
+df_matches = pd.read_csv("databases/matches_FLA2024_processed.csv")
+print(df_matches.head())
